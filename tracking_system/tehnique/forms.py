b6c87b3f5d2d2e17ceda7003f0e2a7d2cf362843
Ivan <vanuartw@mail.ru>
2024-02-22 13:12:47 +0300

    form transport technique

--- tracking_system/tehnique/forms.py
+++ tracking_system/tehnique/forms.py
@@ -1,13 +1,20 @@
 from django import forms
 from .models import Tehnique
+from system_track.models import Building
 
 
 class TransportTechniqueForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         user = args[0].user
-        self.base_fields['tec'] = forms.ModelChoiceField(queryset=Tehnique.objects.filter(building=user.building))
+        self.base_fields['technique'] = forms.ModelChoiceField(label='Техника',
+                                                               queryset=Tehnique.objects.filter(building=user.building,
+                                                                                                status='A'))
+        self.base_fields['technique'].widget.attrs = {'class': 'form-control'}
+        self.base_fields['buildinqs'] = forms.ModelChoiceField(label='Здание куда передать',
+                                                               queryset=Building.objects.exclude(name=user.building))
+        self.base_fields['buildinqs'].widget.attrs = {'class': 'form-control'}
         super(TransportTechniqueForm, self).__init__()
 
     class Meta:
-        fields = ['tec']
+        fields = ['technique']
