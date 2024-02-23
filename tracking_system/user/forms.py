60068f6028510f11f70ba7c9a11c8da73f80df49
Ivan <vanuartw@mail.ru>
2024-02-23 10:48:44 +0300

    template

--- tracking_system/user/forms.py
+++ tracking_system/user/forms.py
@@ -3,20 +3,21 @@ from .models import CustomUser
 
 
 class SignUpUserForm(forms.ModelForm):
-    username = forms.CharField(max_length=100,
+    username = forms.CharField(max_length=100,label='Логин',
                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    first_name = forms.CharField(max_length=100,
+    first_name = forms.CharField(max_length=100,label='Имя',
                                  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(max_length=100,
+    last_name = forms.CharField(max_length=100,label='Фамилия',
                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
-    email = forms.EmailField(max_length=100,
+    email = forms.EmailField(max_length=100,label='Email',
                              widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
-    password = forms.CharField(max_length=100,
+    password = forms.CharField(max_length=100,label='Пароль',
                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Passowrd'}))
 
     def __init__(self, *args, **kwargs):
         self.base_fields['building'].widget.attrs['class'] = 'form-control'
         self.base_fields['building'].required = True
+        self.base_fields['building'].label = 'Здание'
         super(SignUpUserForm, self).__init__(*args, **kwargs)
 
     class Meta:
@@ -25,9 +26,9 @@ class SignUpUserForm(forms.ModelForm):
 
 
 class LoginUserForm(forms.ModelForm):
-    username = forms.CharField(max_length=100,
+    username = forms.CharField(max_length=100, label='Логин',
                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password = forms.CharField(max_length=100,
+    password = forms.CharField(max_length=100,label='Пароль',
                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Passowrd'}))
 
     class Meta:
