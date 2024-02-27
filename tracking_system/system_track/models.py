399443cda5ce878dfb77b0d6cd54a7df4f4031aa
Ivan <vanuartw@mail.ru>
2024-02-27 19:15:05 +0300

    history model

--- tracking_system/system_track/models.py
+++ tracking_system/system_track/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -16,3 +17,30 @@ class Building(models.Model):
         verbose_name_plural = 'Здания'
 
 
+ACTION_CREATE = 'create'
+ACTION_UPDATE = 'update'
+ACTION_DELETE = 'delete'
+
+
+class ChangeLog(models.Model):
+    TYPE_ACTION = [
+        (ACTION_CREATE, 'создание'),
+        (ACTION_DELETE, 'удаление'),
+        (ACTION_UPDATE, 'обновление')
+    ]
+    changed = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    model = models.CharField(max_length=255, verbose_name='tablica', null=True)
+    record_id = models.IntegerField(verbose_name='ID zapisi', null=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, verbose_name='user')
+    action_on_model = models.CharField(choices=TYPE_ACTION, null=True, verbose_name='action')
+    data = models.JSONField(default={}, verbose_name='Изменяемые данные', null=True)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        ordering = ['changed']
+
+
+    @classmethod
+    def add(cls):
--- tracking_system/tehnique/models.py
+++ tracking_system/tehnique/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from system_track.models import Building
 
 
-
 # Create your models here.
 
 
@@ -14,13 +13,13 @@ class Tehnique(models.Model):
     ]
     name = models.CharField(max_length=200, unique=True, verbose_name='Имя')
     inventory_number = models.CharField(max_length=255, unique=True, verbose_name='Инвентарный номер')
-    building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name='tehniques', blank=True, null=True, verbose_name='Здание')
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name='tehniques', blank=True, null=True,
+                                 verbose_name='Здание')
     parent_building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name='parent_tichnique', blank=True,
                                         null=True, verbose_name='Откуда пришло')
     status = models.CharField(max_length=10, choices=CHOICES, default='A', verbose_name='Статус')
     date_crete = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     date_update = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
-
 
     class Meta:
         verbose_name = 'Техника'
